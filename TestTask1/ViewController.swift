98b29ce3ca48278a3d7b874b15db121675b78f8a
Iuliia Vlkv <yu.wlkw@gmail.com>
2021-10-19 16:16:06 +0500

    Initial Commit

---
+++ TestTask1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestTask1
+//
+//  Created by Iuliia Volkova on 19.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
